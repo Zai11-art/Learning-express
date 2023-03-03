7e7b4af0b40c30861f3d4273002957cd9dc8e4f6
Christian Samaniego <66768290+Zai11-art@users.noreply.github.com>
2023-03-04 02:36:23 +0800

    upload learning express

---
+++ Express Studies/index34.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 3000;
+const redditData = require('./data.json')
+console.log(redditData)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/cats', (req,res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', {cats})
+})
+
+// for (let cat of cats) {
+//     console.log(cat);
+// }
+
+app.get('/r/:subreddit', (req,res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
+    // console.log(data)
+    if (data) {
+        res.render(`subreddit`, {...data})
+    } else {
+        res.render(`notfound`, { subreddit })
+    }
+     
+})
+
+app.get('/rand', (req,res) => {
+    const randNum = Math.floor(Math.random()*10) + 1
+    res.render('random', {rand: randNum})
+})
+
+app.listen(PORT, (req,res) =>  {
+    console.log('listening on route')
+})
+
